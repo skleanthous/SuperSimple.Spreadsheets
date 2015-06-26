8ba5a978fb60dcbec177d084516e7c4787a52c8b
Savvas Kleanthous <skleanthous@outlook.com>
2015-06-26 23:31:58 +0100

    dev: Release to GitHub

---
+++ source/SuperSimple.Spreadsheets/Serializer/ISerializerToExcelRow.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SSExcel.Serializer
+{
+    public interface ISerializerToExcelRow
+    {
+        IEnumerable<ExcelRow> Serialize<T>(IEnumerable<T> itemsToSerialize, bool getHeaders = true);
+    }
+}
