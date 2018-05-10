d69d686218450c1f2b0bc7c2a9196bcde5c064be
Richard Baldwin <Richy@parcelvision.com>
2018-05-10 16:29:36 +0100

    Started work on porting project to .NETStandard - currently WIP

---
+++ source/SuperSimple.Spreadsheets.Core/ExcelRow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace SuperSimple.Spreadsheets
+{
+    public class ExcelRow: List<ExcelCell>
+    {
+        public ExcelRow()
+        {
+        }
+
+        public ExcelRow(params object[] values)
+        {
+            foreach (var val in values)
+                this.Add(new ExcelCell(val));
+        }
+
+        /// <summary>
+        /// This is to match iterators
+        /// </summary>
+        /// <param name="values"></param>
+        public ExcelRow(IEnumerable<object> values)
+        {
+            foreach (var val in values)
+                this.Add(new ExcelCell(val));
+        }
+
+        public void AddCell(dynamic value)
+        {
+            this.Add(new ExcelCell(value));
+        }
+    }
+}
---
+++ source/SuperSimple.Spreadsheets.Core/Serializer/ISerializerToExcelRow.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperSimple.Spreadsheets.Serializer
+{
+    public interface ISerializerToExcelRow
+    {
+        IEnumerable<ExcelRow> Serialize<T>(IEnumerable<T> itemsToSerialize, bool getHeaders = true);
+    }
+}
